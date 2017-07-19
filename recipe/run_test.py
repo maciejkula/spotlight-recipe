490fa996bc5ddca19b1f4c56e7766c15ef2edd0d
maciejkula <maciej.kula@gmail.com>
2017-07-19 22:48:26 +0100

    Clone and run tests.

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -1,4 +1,28 @@
-import sys
-print(sys.version)
+import subprocess
+
 
 import spotlight
+
+
+VERSION = getattr(spotlight, '__version__', 'v0.1.0')
+
+
+def run_tests():
+
+    clone = ['git',
+             'clone',
+             'https://github.com/maciejkula/spotlight.git']
+    subprocess.check_call(clone)
+
+    checkout = 'cd spotlight && git checkout tags/{}'.format(VERSION)
+    subprocess.check_call(checkout, shell=True)
+
+    move = 'mv spotlight/tests tests/ && rm -rf spotlight'
+    subprocess.check_call(move, shell=True)
+
+    subprocess.check_call(['py.test', 'tests/'])
+
+    subprocess.check_call('rm -rf tests/', shell=True)
+
+
+run_tests()

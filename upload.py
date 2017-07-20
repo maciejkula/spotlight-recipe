14ce2f33b7a7b5f64c30f188f3c6c1e2929d2c21
maciejkula <maciej.kula@gmail.com>
2017-07-20 20:38:37 +0100

    Add upload script.

---
+++ upload.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import subprocess
+
+import glob
+
+
+def upload():
+
+    for filename in glob.iglob('build/**/spotlight-*.tar.bz2', recursive=True):
+        print('Uploading {}...'.format(filename))
+        subprocess.check_call(['anaconda',
+                               'upload',
+                               filename])
+
+
+if __name__ == '__main__':
+    upload()
